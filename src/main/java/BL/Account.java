be67bcad319426c101b8beefcfe433d7906e05df
Michael Kahr <mikey.kahr@gmail.com>
2019-04-16 21:40:55 +0200

    implemented Account class

--- src/main/java/BL/Account.java
+++ src/main/java/BL/Account.java
@@ -1,4 +1,30 @@
 package BL;
+
 public class Account {
 
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double money) {
+        balance += money;
+    }
+
+    public void withdraw(double money) throws NoMoneyException {
+        if (balance < money) {
+            throw new NoMoneyException();
+        }
+        balance -= money;
+    }
+
+    public void transferTo(Account rec, double money) throws NoMoneyException {
+        withdraw(money);
+        rec.deposit(money);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
